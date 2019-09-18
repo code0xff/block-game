3d99990d566497acc95945533596d9fd6da6ed0a
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2019-09-19 01:43:06 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/', function (req, res) {
+  res.render('index.html');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
